b266aaa5093ec4929b1c798a6ae01cc2b5cd9bfd
Mon R <montserratramirezpino@gmail.com>
2022-05-14 21:45:15 -0400

    Terminado la integracion de carrito de super

--- Neirensal/core/views.py
+++ Neirensal/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render, HttpResponse
 from Neirensal.core.Carrito import Carrito
 from Neirensal.core.models import Producto
 
