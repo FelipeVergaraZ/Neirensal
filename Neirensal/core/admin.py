27b9420c51290b7a61469f9918a3d16ec0b692ec
eltimmy <mart.torresg@duocuc.cl>
2022-05-19 02:59:29 -0400

    los cambios qlos

--- Neirensal/core/admin.py
+++ Neirensal/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Categoria, Remedio
+from .models import Producto, Categoria
+
 # Register your models here.
 
+admin.site.register(Producto)
 admin.site.register(Categoria)
-admin.site.register(Remedio)
--- Neirensal/core/models.py
+++ Neirensal/core/models.py
@@ -1,4 +1,5 @@
 from tabnanny import verbose
+
 from django.db import models
 
 # Create your models here.
@@ -11,15 +12,19 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombreCategoria
 
-#Modelo para remedio
 
-class Remedio(models.Model):
-    id_remedio = models.CharField(max_length=20, verbose_name='Id remedio')
-    nombreRemedio = models.CharField(max_length=50, verbose_name='Nombre remedio')
+class Producto(models.Model):
+    id = models.AutoField(max_length=3, primary_key= True)
+    nombre = models.CharField(max_length=64)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    precio = models.CharField(max_length=20, verbose_name='Precio remedio')
-    marca =  models.CharField(max_length=20, verbose_name='Marca remedio')
+    precio = models.IntegerField()
+    stock = models.CharField(max_length=20, verbose_name='Stock')
+    marca =  models.CharField(max_length=20, verbose_name='Laboratorio')
     descripcion =  models.CharField(max_length=20, verbose_name='Descripcion remedio')
-    
+    estado =  models.CharField(max_length=20, verbose_name='Estado del remedio')
+
     def __str__(self):
-        return self.nombreRemedio
+        return f'{self.nombre} -> {self.precio}'
+
+
+#Modelo para remedio
